99bd3065e71408c69ad5f6860b3a0d7c1fbc6e6c
Axel Biehler <abiehler@capcod.eu>
2025-11-28 15:31:49 +0100

    feat(deploy): add config to deploy

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://lumia.api.test.capcod.cloud',
+  jarvisUrl: 'https://jarvis.api.test.capcod.cloud/api',
+  encPasswordKey: 'FYLsFL6xZI41BX'
+};
